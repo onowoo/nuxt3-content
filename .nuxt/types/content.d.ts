4ca9cca3898f0ad5f241348fe768e0ce99009eeb
onowoo <6559170@qq.com>
2024-03-17 19:37:32 +0800

    111

--- .nuxt/types/content.d.ts
+++
@@ -1,4 +0,0 @@
-declare module '#content/server' {
-  const serverQueryContent: typeof import('/Volumes/Data/nuxt/nuxt3-content/node_modules/.pnpm/@nuxt+content@2.12.1_nuxt@3.10.3_rollup@2.79.1_vue@3.4.21/node_modules/@nuxt/content/dist/runtime/legacy/types').serverQueryContent
-  const parseContent: typeof import('/Volumes/Data/nuxt/nuxt3-content/node_modules/.pnpm/@nuxt+content@2.12.1_nuxt@3.10.3_rollup@2.79.1_vue@3.4.21/node_modules/@nuxt/content/dist/runtime/server').parseContent
-}
