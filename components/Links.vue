0994e0d177cb026c7fd2c80ee0b3b34d58e16aae
onowoo <6559170@qq.com>
2024-03-18 14:49:28 +0800

    formatter link json data

--- components/Links.vue
+++ components/Links.vue
@@ -1,35 +1,60 @@
 <template>
-  <div class="grid grid-cols-2 mx-6 lg:mx-auto lg:max-w-4xl gap-6 md:grid-cols-6 mt-20 lg:mt-25">
-    <ContentQuery :path="$route.path" find="one" v-slot="{ data }">
+<div class="lg:max-w-4xl mx-auto mt-20 lg:mt-25">
+  <ContentQuery :path="$route.path" find="one" v-slot="{ data }">
+    <div v-for="item in data.body" :key="item">
+      <h2 class="my-4 mx-6 lg:mx-auto lg:max-w-4xl">{{ item.name }}</h2>
       <div
-        v-for="link in data.body"
-        :key="link.id"
-        class="documentation-container relative order-last col-span-2 row-span-2 max-w-110 items-center rounded-xl text-black shadow lg:order-none lg:col-span-2 dark:border-transparent hover:border-transparent dark:text-white"
+        class="grid grid-cols-2 mx-6 lg:mx-auto lg:max-w-4xl gap-6 md:grid-cols-6"
       >
         <div
-          class="gradient-border gradient-border-square gradient-border-documentation h-40"
-        />
-        <div
-          flex="~ col gap-2"
-          class="h-40 p-3 rounded-xl bg-white lg:flex-col dark:(bg-gray-900/60 hover:bg-[#0c0f27] border-gray-800) border border-gray-200 hover:border-transparent"
+          v-for="link in item.list"
+          :key="link.id"
+          class="documentation-container relative order-last col-span-2 row-span-2 max-w-110 items-center rounded-xl text-black shadow lg:order-none lg:col-span-2 dark:border-transparent hover:border-transparent dark:text-white"
         >
-          <div class="line-clamp-1 leading-7">{{ link.title }}</div>
-          <div text="xs more gray-500" class="line-clamp-3 leading-6" flex="1">{{ link.description }}</div>
-          <div flex="~ justify-between items-center" text="xs gray-500">
-            <div>{{ link.category }}</div>
-            <div flex="~ gap-2">
-              <NuxtLink :to="link.site" target="_blank"><el-tag type="info" size="small" effect="plain">{{ `官网` }}</el-tag></NuxtLink>
-              <NuxtLink :to="link.git" target="_blank"><el-tag type="info" size="small" effect="plain">{{ `仓库` }}</el-tag></NuxtLink>
-              <NuxtLink :to="link.doc" target="_blank"><el-tag type="info" size="small" effect="plain">{{ `文档` }}</el-tag></NuxtLink>
+          <div
+            class="gradient-border gradient-border-square gradient-border-documentation h-40"
+          />
+          <div
+            flex="~ col gap-2"
+            class="h-40 p-3 rounded-xl bg-white lg:flex-col dark:(bg-gray-900/60 hover:bg-[#0c0f27] border-gray-800) border border-gray-200 hover:border-transparent"
+          >
+            <div class="line-clamp-1 leading-7">{{ link.title }}</div>
+            <div
+              text="xs more gray-500"
+              class="line-clamp-3 leading-6"
+              flex="1"
+            >
+              {{ link.description }}
+            </div>
+            <div flex="~ justify-between items-center" text="xs gray-500">
+              <div>{{ link.category }}</div>
+              <div flex="~ gap-2">
+                <NuxtLink :to="link.site" target="_blank"
+                  ><el-tag type="info" size="small" effect="plain">{{
+                    `官网`
+                  }}</el-tag></NuxtLink
+                >
+                <NuxtLink :to="link.git" target="_blank"
+                  ><el-tag type="info" size="small" effect="plain">{{
+                    `仓库`
+                  }}</el-tag></NuxtLink
+                >
+                <NuxtLink :to="link.doc" target="_blank"
+                  ><el-tag type="info" size="small" effect="plain">{{
+                    `文档`
+                  }}</el-tag></NuxtLink
+                >
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </ContentQuery>
-  </div>
+    </div>
+  </ContentQuery>
+</div>
 </template>
 <script setup>
-const { path } = useRoute()
+const { path } = useRoute();
 </script>
 
 <style scoped>
@@ -125,10 +150,10 @@ const { path } = useRoute()
   }
 }
 .more {
-    -webkit-line-clamp: 2;
-    display: -webkit-box;
-    -webkit-box-orient: vertical;
-    overflow: hidden;
-    text-overflow: ellipsis;
+  -webkit-line-clamp: 2;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
 }
 </style>
