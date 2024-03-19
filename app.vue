753f3c28d8dba0ba7e56b989c210cbf941c4c860
onowoo <6559170@qq.com>
2024-03-19 15:29:23 +0800

    a

--- app.vue
+++ app.vue
@@ -6,7 +6,19 @@
   </NuxtLayout>
 </div>
 </template>
-
+<script setup>
+useHead({
+  script: [ { innerHTML: `
+  var _hmt = _hmt || [];
+  (function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?42ad212ec8fd33be90a6104d3afa4ae4";
+    var s = document.getElementsByTagName("script")[0]; 
+    s.parentNode.insertBefore(hm, s);
+  })();
+  ` } ]
+})
+</script>
 <style>
 html,
 body,
